e3ef457681c1c21b83dfccba0c0e6357ca3d0c7b
famous okolocha <okolochafamous749@gmail.com>
2026-06-07 23:37:18 +0100

    refactor:connect frontend and backedend signup feature

--- client/src/pages/signup.jsx
+++ client/src/pages/signup.jsx
@@ -1,8 +1,10 @@
+import api from "../services/api"; // allows us to access api from ./services/api.js
+
 import { useState } from "react";     //This import useState from react which lets React store and update data inside the component.
 
 function Signup() {                  // This creates our signup page component, everything inside here is what will show and do
   const [form, setForm] = useState({    //This creates state (memory) for the form  form =initial value setForm=function to update it 
-    username: "",
+   
     email: "",
     password: ""
   });
@@ -14,9 +16,16 @@ function Signup() {                  // This creates our signup page component, 
     });
   };
 
-  const handleSubmit = (e) => {         //Runs when user clicks “Sign Up”
+  const handleSubmit = async (e) => {         //Runs when user clicks “Sign Up”
     e.preventDefault();                 // Stops page from reloading (default browser behavior)
-    console.log(form);                 //Prints form data to console (for testing)
+   
+    try {
+      const response =await api.post("/auth/signup", form);
+      console.log(response.data);
+    }
+    catch(error){
+       console.log(err.response?.data || err.message);
+    }
   };
 
   return (                           // this is what shows on our screen
@@ -24,11 +33,6 @@ function Signup() {                  // This creates our signup page component, 
       <h2>Signup</h2>
                                                                 
       <form onSubmit={handleSubmit}>           
-        <input                                         //When form is submitted, run handleSubmit
-          name="username"                              //connects it to form.username
-          placeholder="Username"                 
-          onChange={handleChange}                      //updates state when typing
-        />
 
         <input
           name="email"                
